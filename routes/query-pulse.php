b08004ea0716e859573958ce8c90a516096bebf5
alfinfaturochman <tamael.pradhita@gmail.com>
2026-04-04 14:32:13 +0700

    Fix: handle unauthorized users accessing dashboard

--- routes/query-pulse.php
+++ routes/query-pulse.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Alfinprdht\QueryPulse\Controllers\DashboardController;
 
-Route::middleware(['web'])
+Route::middleware(['web', 'auth'])
     ->prefix('query-pulse')
     ->name('query-pulse.')
     ->group(function () {
